9c14b6d7ba790823ca6b290ef80b99d83f509560
greenywd <greenydev@greenmail.net.au>
2019-05-16 09:39:08 +0930

    Removed Global.swift and its references

--- Watchlist/View Controllers/SettingsViewController.swift
+++ Watchlist/View Controllers/SettingsViewController.swift
@@ -15,18 +15,11 @@ class SettingsViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		resetButton.addTarget(self, action: #selector(resetPrefs), for: .touchUpInside)
-		
 		// Do any additional setup after loading the view.
 	}
 	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
-	}
-	
-    @objc func resetPrefs() {
-		print("removing favourite shows...")
-		userDefaults.removeObject(forKey: "favouriteShows")
 	}
 }
