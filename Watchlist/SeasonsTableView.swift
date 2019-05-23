bceacc4dfff04f729db045342f0a65b5f1c070ee
greenywd <greenydev@greenmail.net.au>
2019-05-23 11:53:28 +0930

    Begin creating a "Seasons" TableView instead of having 1 giant list of episodes

---
+++ Watchlist/SeasonsTableView.swift
@@ -0,0 +1,21 @@
+//
+//  SeasonsTableView.swift
+//  Watchlist
+//
+//  Created by Thomas Greenwood on 16/5/19.
+//  Copyright © 2019 Thomas Greenwood. All rights reserved.
+//
+
+import UIKit
+
+class SeasonsTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
