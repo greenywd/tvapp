7a921571bc9715aec28ed0328fe0dfd3d98a3203
greenywd <greenydev@greenmail.net.au>
2019-05-30 21:46:45 +0930

    Moved the SearchTableViewCell into a custom class + xib named ShowTableViewCell

---
+++ Watchlist/Custom Cells/ShowTableViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  SearchTableViewCell.swift
+//  Watchlist
+//
+//  Created by Thomas Greenwood on 11/5/19.
+//  Copyright © 2019 Thomas Greenwood. All rights reserved.
+//
+
+import UIKit
+
+class ShowTableViewCell: UITableViewCell {
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet var visualEffectView: UIVisualEffectView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var detailLabel: UILabel!
+    
+    var show: Show? { didSet { updateCell() } }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        show = nil
+    }
+    
+    func updateCell() {
+        if let show = show {
+            titleLabel.text = show.seriesName
+            detailLabel.text = show.overview
+            
+            if let url = URL(string: "https://www.thetvdb.com/banners/" + show.banner!) {
+
+                if backgroundImageView.image == nil {
+                    DispatchQueue.global(qos: .background).async {
+                        let dataForImage = try? Data(contentsOf: url)
+
+                        DispatchQueue.main.async {
+                            if let image = dataForImage {
+                                self.backgroundImageView.image = UIImage(data: image)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
