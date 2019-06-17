8e824de84455e96aae7b2638d6c6fd23f85982e0
greenywd <greenydev@greenmail.net.au>
2019-06-17 17:55:20 +0930

    Updated most views to adapt to dark mode

--- Watchlist/AppDelegate.swift
+++ Watchlist/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        window?.backgroundColor = .systemBackground
         TVDBAPI.retrieveToken()
         
         return true
