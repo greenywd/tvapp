28ce65252faf229f0ae53ae7ed43c8983236cd88
greenywd <greenydev@greenmail.net.au>
2019-06-21 18:32:52 +0930

    [SeasonsVC] Implemented UITableView's `didSelectRowAt` with `deselectRow`

--- Watchlist/ShowSeasonsTableViewController.swift
+++ Watchlist/ShowSeasonsTableViewController.swift
@@ -77,6 +77,10 @@ class ShowSeasonsTableViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     /*
      // Override to support conditional editing of the table view.
      override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
