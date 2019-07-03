60c7418f73aebf59a626645c8d10d67fcb084880
greenywd <greenydev@greenmail.net.au>
2019-07-03 15:00:20 +0930

    [EpisodeVC] Added a VC for episode details

--- Watchlist/View Controllers/EpisodeTableViewController.swift
+++ Watchlist/View Controllers/EpisodeTableViewController.swift
@@ -6,85 +6,106 @@
 //  Copyright © 2019 Thomas Greenwood. All rights reserved.
 //
 
+import Foundation
 import UIKit
+import CoreData
 
 class EpisodeTableViewController: UITableViewController {
-
+    
+    //MARK: - Properties
+    
+    // var activityIndicator: UIActivityIndicatorView?
+    @IBOutlet var bannerImageView: SeasonsHeaderImageView?
+    @IBOutlet weak var bannerImageCell: UITableViewCell!
+    @IBOutlet weak var showDescription: UITextView!
+    @IBOutlet weak var showMoreButton: UIButton!
+    @IBOutlet var showDescriptionHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var showDescriptionBottomConstraint: NSLayoutConstraint!
+    
+    var episode: Episode! {
+        didSet {
+            DispatchQueue.main.async {
+                if (self.showDescription.frame.height < 150) {
+                    
+                    if let btn = self.showMoreButton {
+                        btn.removeFromSuperview()
+                        self.showDescriptionBottomConstraint.constant = 0
+                    }
+                    
+                    self.showDescriptionHeightConstraint.isActive = false
+                }
+                self.tableView.reloadRows(at: [(IndexPath(row: 0, section: 0))], with: .none)
+            }
+        }
+    }
+    var headerImage: UIImage? {
+        didSet {
+            self.bannerImageView?.image = self.headerImage
+        }
+    }
+    
+    //MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        // print(showDescriptionHeightConstraint.constant)
+        navigationItem.title = episode.episodeName
+        showDescription.text = episode.overview
+        showDescription.textContainer.lineBreakMode = .byTruncatingTail
+        showMoreButton.addTarget(self, action: #selector(expandTextView), for: .touchUpInside)
+        
+        if let image = headerImage {
+            bannerImageView?.image = image
+        } else {
+            DispatchQueue.global().async {
+                let data = try? Data(contentsOf: URL(string: "https://www.thetvdb.com/banners/" + self.episode.filename!)!)
+                
+                DispatchQueue.main.async {
+                    if let image = data {
+                        let banner = UIImage(data: image)
+                        self.bannerImageView?.image = banner
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+        }
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.layoutMargins = .zero
+        tableView.reloadData()
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    @objc func expandTextView() {
+        if (showDescriptionHeightConstraint.isActive) {
+            showDescriptionHeightConstraint.isActive = false
+            showMoreButton.removeFromSuperview()
+            self.showDescriptionBottomConstraint.constant = 0
+            tableView.reloadRows(at: [IndexPath(row: 0, section: 0)], with: .none)
+        }
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        switch segue.destination {
+        case let episodeVC as ShowEpisodeViewController:
+            if (segue.identifier == "segueToEpisode") {
+                episodeVC.id = episode.id
+            }
+            
+        case let seasonsVC as ShowSeasonsTableViewController:
+            if (segue.identifier == "showToSeasonEpisode") {
+                seasonsVC.showID = episode.id
+            }
+            
+        default:
+            preconditionFailure("Expected a ShowEpisodeViewController or ShowSeasonsTableViewController")
+        }
     }
-    */
+}
 
+extension EpisodeTableViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // Segue performed in Storyboard
+    }
 }
