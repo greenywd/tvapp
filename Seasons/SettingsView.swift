d26cdf2ba3ae99c3a38aa70cf30aef1d49d86697
greenywd <greenydev@greenmail.net.au>
2019-08-12 17:40:32 +0930

    [SettingsVC] Added Notifications and Support menus

--- Seasons/SettingsView.swift
+++ Seasons/SettingsView.swift
@@ -12,8 +12,10 @@ import Combine
 final class ThemeChanger: Identifiable, ObservableObject {
     var didChange = PassthroughSubject<ThemeChanger, Never>()
     
+    // FIXME: Save the theme preference, and load upon app startup. Currently this setting applies, but isn't remembered between app loads.
     var selection: Int = (UIApplication.shared.windows.first?.traitCollection.userInterfaceStyle.rawValue)! {
         didSet {
+            print("Currently: \(selection)")
             if selection == 0 {
                 UIApplication.shared.windows.first?.overrideUserInterfaceStyle = .unspecified
             } else if selection == 1 {
@@ -34,12 +36,13 @@ struct SettingsView: View {
         var description: String {
             get {
                 switch self {
+                case .automatic:
+                    return "Automatic"
                 case .light:
                     return "Light"
                 case .dark:
                     return "Dark"
-                case .automatic:
-                    return "Automatic"
+                    
                 }
             }
         }
@@ -61,15 +64,27 @@ struct SettingsView: View {
                 }
                 
                 VStack (alignment: .leading) {
-                    NavigationLink(destination: SettingsDebugView()) {
+                    NavigationLink(destination: SettingsNotificationsView()) {
                         VStack (alignment: .leading) {
-                            Text("Debugging")
+                            Text("Notifications")
                                 .font(.headline)
-                            Text("If you're having issues, try some of the actions here.")
+                            Text("Customise how you recieve notifications.")
                                 .font(.subheadline)
                         }
                     }
                 }
+                
+                VStack (alignment: .leading) {
+                    NavigationLink(destination: SettingsSupportView()) {
+                        VStack (alignment: .leading) {
+                            Text("Support")
+                                .font(.headline)
+                            Text("Get some help here!")
+                                .font(.subheadline)
+                        }
+                    }
+                }
+                
                 VStack (alignment: .leading) {
                     NavigationLink(destination: SettingsAboutView()) {
                         VStack (alignment: .leading) {
@@ -85,13 +100,62 @@ struct SettingsView: View {
     }
 }
 
+// MARK: Notifications View
+struct SettingsNotificationsView: View {
+    @State var recieveUpdateNotification = true
+    var body: some View {
+        List {
+            VStack (alignment: .leading) {
+                Toggle(isOn: $recieveUpdateNotification) {
+                    Text("Show Updates")
+                        .font(.headline)
+                }
+                Text("Receive a notification when a favourite show is updated with new information, i.e. new episodes.")
+                    .font(.subheadline)
+            }
+            .navigationBarTitle("Notifications")
+        }
+    }
+}
+
+// MARK: Support View
+struct SettingsSupportView: View {
+    var body: some View {
+        List {
+            Text("Before doing anything, check to see whether your issue is known!")
+                .font(.subheadline)
+            
+            Button(action: { PersistenceService.dropTable() }) {
+                Text("Known Issues")
+                    .foregroundColor(Color.accentColor)
+            }
+            
+            VStack (alignment: .leading) {
+                NavigationLink(destination: SettingsDebugView()) {
+                    VStack (alignment: .leading) {
+                        Text("Debugging")
+                            .font(.headline)
+                        Text("If you're having issues, try some of the actions here.")
+                            .font(.subheadline)
+                    }
+                }
+            }
+        }
+        .navigationBarTitle("Support")
+    }
+}
+
 // MARK: Debug View
 struct SettingsDebugView: View {
     var body: some View {
         List {
-            Button(action: { PersistenceService.dropTable() }) {
-                Text("Remove all Favourites")
-                    .foregroundColor(Color.blue)
+            VStack (alignment: .leading) {
+                Button(action: { PersistenceService.dropTable() }) {
+                    Text("Remove all Favourites")
+                        .foregroundColor(Color.orange)
+                }
+                Text("This does exactly what it sounds like. Proceed with caution")
+                    .font(.footnote)
             }
         }
         .navigationBarTitle("Debugging")
@@ -106,7 +170,12 @@ struct SettingsAboutView: View {
                 Text("Privacy Policy")
                     .font(.headline)
             }
-            Text("Version 1.0").foregroundColor(Color.secondary)
+            NavigationLink(destination: SettingsCreditsView()) {
+                Text("Credits")
+                    .font(.headline)
+            }
+            Text("Version 1.0")
+                .foregroundColor(Color.secondary)
         }
         .navigationBarTitle("About")
     }
@@ -116,11 +185,24 @@ struct SettingsAboutView: View {
 struct SettingsPrivacyPolicyView: View {
     var body: some View {
         VStack {
-            Text("The privacy policy is simple: no personal data shared with us will be given to any third party, under any circumstances. Your data will also never be used by us for any purpose without specific permission.")
-            Text("Seasons itself engages in no ad targeting, data mining, or other activities that may compromise your privacy, however ")
+            Text("The privacy policy is simple: no personal data shared with myself will be given to any third party, under any circumstances. By default, no data is collected by Seasons. However, information (such as crash reports) may be collected to improve Seasons based on your device settings. Seasons retrieves data from thetvdb.com, who may collect and use your data in accordance with their privacy policy. Seasons itself engages in no ad targeting, data mining, or other activities that may compromise your privacy.")
+                .padding()
             Spacer()
         }
         .navigationBarTitle("Privacy Policy")
+    }
+}
+
+// MARK: Credits View
+struct SettingsCreditsView: View {
+    var body: some View {
+        VStack {
+            Text("Thanks to:")
+                .font(.headline)
+            
+            Spacer()
+        }
+        .navigationBarTitle("Credits")
     }
 }
 
