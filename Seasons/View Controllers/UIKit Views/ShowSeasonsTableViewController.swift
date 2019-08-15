b525aa7db5777d373ca492d2aaaaf57f55635623
greenywd <greenydev@greenmail.net.au>
2019-08-15 20:30:00 +0930

    [SeasonsVC] Fixed episodes not showing when being retrieved from CoreData

--- Seasons/View Controllers/UIKit Views/ShowSeasonsTableViewController.swift
+++ Seasons/View Controllers/UIKit Views/ShowSeasonsTableViewController.swift
@@ -23,6 +23,7 @@ class ShowSeasonsTableViewController: UITableViewController {
         
         if let episodes = PersistenceService.getEpisodes(show: showID) {
             self.episodes = episodes
+            self.seasons = Set(episodes.map { $0.airedSeason ?? 999 }).sorted()
         } else {
             TVDBAPI.getEpisodes(show: showID) { (episodeList) in
                 if let episodes = episodeList {
@@ -37,12 +38,6 @@ class ShowSeasonsTableViewController: UITableViewController {
                 }
             }
         }
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     // MARK: - Table view data source
@@ -74,41 +69,6 @@ class ShowSeasonsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
     
     // MARK: - Navigation
     
