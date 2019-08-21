1d9b683f6132ffe68d8489a5e644ebb8fa67d815
greenywd <greenydev@greenmail.net.au>
2019-08-21 15:59:10 +0930

    [SettingsVC] Moved our UserDefaults wrapper and ThemeChanger to their own files.

---
+++ Seasons/View Controllers/SwiftUI Views/UserDefaultsWrapper.swift
@@ -0,0 +1,53 @@
+//
+//  UserDefaultsWrapper.swift
+//  Seasons
+//
+//  Created by Thomas Greenwood on 21/8/19.
+//  Copyright © 2019 Thomas Greenwood. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+@propertyWrapper
+struct UserDefault<T> {
+    let key: String
+    let defaultValue: T
+    
+    init(_ key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+    
+    var wrappedValue: T {
+        get {
+            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+}
+
+final class UserSettings: ObservableObject {
+    let didChange = PassthroughSubject<Void, Never>()
+    
+    @UserDefault("theme", defaultValue: 0)
+    var theme: Int {
+        didSet {
+            didChange.send()
+        }
+    }
+    @UserDefault("showUpdateNotification", defaultValue: true)
+    var showUpdateNotification: Bool {
+        didSet {
+            didChange.send()
+        }
+    }
+    @UserDefault("preferFullHD", defaultValue: true)
+    var preferFullHD: Bool {
+        didSet {
+            didChange.send()
+        }
+    }
+}
