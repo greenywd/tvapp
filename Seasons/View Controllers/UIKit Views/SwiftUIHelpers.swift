33f85671021eb68fa24567a235d6587c184ad694
greenywd <greenydev@greenmail.net.au>
2019-08-31 13:12:40 +0930

    [Seasons] Added SwiftUIHelper class

--- Seasons/View Controllers/UIKit Views/SwiftUIHelpers.swift
+++ Seasons/View Controllers/UIKit Views/SwiftUIHelpers.swift
@@ -1,15 +1,15 @@
 //
-//  SettingsViewController.swift
-//  Watchlist
+//  SwiftUIHelpers.swift
+//  Seasons
 //
-//  Created by Thomas Greenwood on 26/2/17.
-//  Copyright © 2017 Thomas Greenwood. All rights reserved.
+//  Created by Thomas Greenwood on 31/8/19.
+//  Copyright © 2019 Thomas Greenwood. All rights reserved.
 //
 
 import UIKit
 import SwiftUI
 
-class SettingsViewController: UIViewController {
+class SwiftUIHelpers : UIViewController {
     @IBSegueAction func showSettingsView(_ coder: NSCoder) -> UIViewController? {
         return UIHostingController(coder: coder, rootView: SettingsView())
     }
