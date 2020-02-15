9430195d77af693a21de4048a8478f22d9ada40c
greenywd <greenydev@greenmail.net.au>
2020-02-15 16:16:22 +1030

    Begun removing SwiftUI because I dread having to continue using it

---
+++ Seasons/View Controllers/UIKit Views/SettingsRootViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsRootViewController.swift
+//  Seasons
+//
+//  Created by Thomas Greenwood on 15/2/20.
+//  Copyright © 2020 Thomas Greenwood. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsRootViewController : UITableViewController {
+    
+}
