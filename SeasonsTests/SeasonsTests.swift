441399a01079efcc145d9a2db5375126898ecf71
greenywd <greenydev@greenmail.net.au>
2020-03-13 22:15:05 +1030

    Add empty test

---
+++ SeasonsTests/SeasonsTests.swift
@@ -0,0 +1,35 @@
+//
+//  SeasonsTests.swift
+//  SeasonsTests
+//
+//  Created by Thomas Greenwood on 13/3/20.
+//  Copyright © 2020 Thomas Greenwood. All rights reserved.
+//
+
+import XCTest
+
+@testable import Seasons
+
+class SeasonsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
