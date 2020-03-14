224d35dcedaae2c2a58982ea8d44ae362b25d89b
greenywd <greenydev@greenmail.net.au>
2020-03-14 13:15:22 +1030

    Add basic JSON Decode Unit Test

--- SeasonsTests/SeasonsTests.swift
+++ SeasonsTests/SeasonsTests.swift
@@ -11,25 +11,27 @@ import XCTest
 @testable import Seasons
 
 class SeasonsTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    
+    func testJSONDecoding() throws {
+        if let path = Bundle(for: type(of: self)).path(forResource: "Show", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let json = try JSONDecoder().decode(RawShowResponse.self, from: data)
+                XCTAssert(json.data != nil, "Failed to decode JSON.")
+                
+            } catch {
+                XCTFail("Failed to create data from JSON file.")
+            }
+        } else {
+            XCTFail("Failed to retrieve JSON file.")
         }
     }
-
 }
